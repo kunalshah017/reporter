5027eb8e8e5d4420dcb4b75c725cfde5fee4b97a
Kunal Shah <kunalmanishshah@gmail.com>
2024-08-24 10:24:33 +0530

    react+vite setup

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      "prettier",
+    ],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "prettier/prettier": ["error"],
+      "linebreak-style": 0,
+      "func-names": 0,
+      "class-methods-use-this": 0,
+      "no-param-reassign": 0,
+      "no-underscore-dangle": "off",
+      indent: "off",
+      "padding-line-between-statements": [
+        "error",
+        {
+          blankLine: "always",
+          prev: ["const", "let", "var", "if", "case"],
+          next: "*",
+        },
+        {
+          blankLine: "any",
+          prev: ["const", "let", "var"],
+          next: ["const", "let", "var"],
+        },
+      ],
+    },
+  },
+);
